41d8c89c27ba7c9d946555faa7d0efb34c44d0a5
germano <germanovct@gmail.com>
2023-04-03 19:36:53 -0300

    Primera pre entrega + motores de plantilla + websocket

--- src/socket.js
+++ src/socket.js
@@ -13,7 +13,7 @@ socket.connect = function(httpServer) {
     let { io } = socket;
 
     io.on("connection", (socket) => {
-        console.log(`${socket.id} connected`);
+        console.log(`${socket.id} conectado`);
         io.emit("products", products);
     });
 };
