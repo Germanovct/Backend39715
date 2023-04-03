f0efb0b42cf53d417f1fda3d52ce4c302b0cf580
germano <germanovct@gmail.com>
2023-04-02 22:36:46 -0300

    Primera pre entrega + motores de plantilla + websocket

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,16 +1,17 @@
-import { Router } from 'express';
+import { Router } from "express";
+import ProductManager from "../ProductManager.js";
 
 const router = Router();
+const productManager = new ProductManager();
 
+const products = await productManager.getProducts();
 
-router.get('/', (req, res) => {
-  res.render('home');
+router.get("/", async (req, res) => {
+  res.render("home", { products, style: "styles.css", title: "Products" });
 });
 
-
 router.get('/realtimeproducts', (req, res) => {
-  res.render('realTimeProducts');
+  res.render('realTimeProducts', { products, style: "styles.css", title: "Products" });
 });
 
 export default router;
-
